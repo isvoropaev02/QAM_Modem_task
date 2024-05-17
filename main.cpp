fabd42d974339eb25d5d9be9ab1a91f3ac0a5ee0
isvoropaev02 <is.voropaev@gmail.com>
2024-05-17 20:02:15 +0300

    deleted_unused_code_parts

--- main.cpp
+++ main.cpp
@@ -13,10 +13,12 @@ vector<int> transmitt_bits(vector<int> &message, Modulator mod, double var)
     int len_mes = message.size();
     if (len_mes % 2 == 0 && len_mes % 4 == 0 && len_mes % 6 == 0)
     {
-        vector<vector<double>> modulated_signal = mod.modulate(message);
-        //print_qam_vector(modulated_signal);
+        // использование модулятора
+        vector<vector<double>> modulated_signal = mod.modulate(message); // вектор с элементами I и Q
+        // добавление шума
         AWGN noise(var);
         noise.add_noise(modulated_signal);
+        // использование демодулятора
         Demodulator dem(mod);
         return dem.demodulate(modulated_signal);
     }
@@ -34,8 +36,8 @@ int main()
     
     // сначала сгенерируем сообщение и посмотрим его
     vector<int> mes = generate_bit_message(num_bits);
-    //print_bits_vector(mes);
 
+    // будем записывать данные для графика в файл
     ofstream file("bit_errors.txt");
     if (file.is_open())
     {
@@ -44,57 +46,19 @@ int main()
         for (auto var : var_values)
         {
             // смоделируем передачу сообщения из битов
-            vector<int> result_qam4 = transmitt_bits(mes, Modulator(4), var);
+            vector<int> result_qam4 = transmitt_bits(mes, Modulator(4), var); // вектор из битов на выходе
             vector<int> result_qam16 = transmitt_bits(mes, Modulator(16), var);
             vector<int> result_qam64 = transmitt_bits(mes, Modulator(64), var);
             // считаем количество ошибочно декодированных битов
             int biterr_qam4 = calculate_biterr(mes, result_qam4);
             int biterr_qam16 = calculate_biterr(mes, result_qam16);
             int biterr_qam64 = calculate_biterr(mes, result_qam64);
-
+            // записываем в файл
             file << var << "," << ((double) biterr_qam4 / num_bits) << "," << ((double) biterr_qam16 / num_bits)
                  << "," << ((double) biterr_qam64 / num_bits) << "\n";
         }
         file.close();
     }
-    else
-    {
-        cout << "Error - File is not opened"
-             << "\n";
-    }
-
-    // cout << "QAM4" <<"\n";
-    // cout << "biterr=" << calculate_biterr(mes, result_qam4) << "\n";
-    // print_bits_vector(result_qam4);
-    // cout << "QAM16" <<"\n";
-    // cout << "biterr=" << calculate_biterr(mes, result_qam16) << "\n";
-    // print_bits_vector(result_qam16);
-    // cout << "QAM64" <<"\n";
-    // cout << "biterr=" << calculate_biterr(mes, result_qam64) << "\n";
-    // print_bits_vector(result_qam64);
-
-
-    // посмотреть точки на созвездии модулятора
-    // Modulator mod(16);
-    // vector<vector<double>> modulated_signal = mod.modulate(mes);
-    // AWGN noise(0.005);
-    // noise.add_noise(modulated_signal);
-    // ofstream file2("signal_on_constellation.txt");
-    // if (file2.is_open())
-    // {
-    //     file2 << "I,Q"
-    //          << "\n";
-    //     for (size_t i = 0; i < modulated_signal.size(); i++)
-    //     {
-    //         file2 << modulated_signal[i][0] << "," << modulated_signal[i][1] << "\n";
-    //     }
-    //     file2.close();
-    // }
-    // else
-    // {
-    //     cout << "Error - File is not opened"
-    //          << "\n";
-    // }
-    
+    else { cout << "Error - File is not opened" << "\n"; }
     return 0;
 }
